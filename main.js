45987df81e9ad5e4b6e01f2d3ecc085c63a8bb72
sanju12327 <80778249+sanju12327@users.noreply.github.com>
2021-12-12 10:47:53 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	canvas.parent('canvas');
+}
+
+function draw() {
+	game()
+}
+
+
+
+
